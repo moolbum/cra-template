a9fcda83bb315d4c1c27f99eab7a8f1f449b935d
slogup-dale <dyddn304@gmail.com>
2024-03-15 16:52:13 +0900

    build: 초기 라이브러리 설치

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120, // 코딩에서 단락을 길이
+  tabWidth: 2, // 탭너비 2로 설정
+  endOfLine: 'lf', // EoF 방식, OS별로 처리 방식이 다름
+  arrowParens: 'avoid', // 화살표 함수 괄호 사용 방식
+  singleQuote: true, // 큰따옴표를 작은따옴표로 자동수정
+  semi: true, // 세미콜론 자동수정
+  trailingComma: 'es5', // 후행 쉼표
+};
---
+++ craco.config.js
@@ -0,0 +1,29 @@
+const CracoAlias = require('craco-alias');
+const CracoEnvPlugin = require('craco-plugin-env');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
+          return webpackConfig;
+        },
+      },
+    },
+    {
+      plugin: CracoEnvPlugin,
+      options: {
+        variables: {},
+      },
+    },
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.paths.json',
+      },
+    },
+  ],
+};
