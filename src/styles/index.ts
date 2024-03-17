27f3766fa5b4e5bf69400e7623572cca1432ee3e
slogup-dale <dyddn304@gmail.com>
2024-03-17 16:47:02 +0900

    refactor: theme 파일 수정

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,9 +1,9 @@
 export { default as GlobalStyle } from './globalStyle';
 export { getFontFamily, fontWeights, typography } from './typography';
 export { zIndex } from './zIndex';
-export { darkMode, lightMode } from './color';
+export { darkMode, lightMode } from './theme';
 export { MediaQuery, breakpoints } from './mediaQuery';
 
 export type { TypographyType } from './typography';
 export type { ZIndexType } from './zIndex';
-export type { ColorTypes, Colors, ThemeTypes } from './color';
+export type { ColorTypes, Colors, ThemeTypes } from './theme';
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,17 @@
+import { fontWeights } from './typography';
+import { zIndex } from './zIndex';
+
+export const commonTheme = {
+  zIndex,
+  fontWeights,
+};
+
 export const darkMode = {
   colors: {
     white: '#000000',
     black: '#ffffff',
   },
+  ...commonTheme,
 };
 
 export const lightMode = {
@@ -10,6 +19,7 @@ export const lightMode = {
     white: '#ffffff',
     black: '#000000',
   },
+  ...commonTheme,
 };
 
 export type ColorTypes = typeof lightMode;
