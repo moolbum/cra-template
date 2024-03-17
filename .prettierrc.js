a556f25c0283a46037bcbd59b84038a5574d5faa
slogup-dale <dyddn304@gmail.com>
2024-03-17 16:31:45 +0900

    chore: prettier 수정

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,5 +5,5 @@ module.exports = {
   arrowParens: 'avoid', // 화살표 함수 괄호 사용 방식
   singleQuote: true, // 큰따옴표를 작은따옴표로 자동수정
   semi: true, // 세미콜론 자동수정
-  trailingComma: 'es5' // 후행 쉼표
+  trailingComma: 'es5', // 후행 쉼표
 };
