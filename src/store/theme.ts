5fad2315b77e973e736c0333cdd7cb71b9c1aa93
slogup-dale <dyddn304@gmail.com>
2024-03-17 17:12:24 +0900

    feat: zustand persist middleware 추가

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { ThemeTypes, darkMode, lightMode } from '@styles/index';
 
 type ThemeState = {
@@ -27,5 +28,41 @@ const useTheme = create<ThemeState>(set => ({
       })),
   },
 }));
+
+export const useThemePersistTest = create(
+  persist(
+    set => ({
+      theme: { value: lightMode },
+      themeActions: {
+        lightMode: () =>
+          set(() => ({
+            theme: { value: lightMode },
+          })),
+        darkMode: () =>
+          set(() => ({
+            theme: { value: darkMode },
+          })),
+        themeToggle: () =>
+          set(
+            (state: {
+              theme: {
+                value: {
+                  zIndex: { switch: number; dialogContent: number; alertDialogContent: number; toast: number };
+                  fontWeights: { regular: number; medium: number; semibold: number; bold: number };
+                  colors: { white: string; black: string };
+                };
+              };
+            }) => ({
+              theme: { value: state.theme.value === lightMode ? darkMode : lightMode },
+            })
+          ),
+      },
+    }),
+    {
+      name: 'persist-theme',
+      getStorage: () => localStorage,
+    }
+  )
+);
 
 export default useTheme;
